482f3392974a008341a39f4558b91b5ceb520426
Lucas Servén Marín <lserven@gmail.com>
2020-11-02 15:29:30 +0100

    binomi.al: migrate away from mailgun
    Signed-off-by: Lucas Servén Marín <lserven@gmail.com>

--- binomi.al/dns.tf
+++ binomi.al/dns.tf
@@ -1,5 +1,5 @@
 locals {
-  domain = "binomi.al"
+  domain  = "binomi.al"
   zone_id = "6351769cb776c364d2b267fd9a662208"
 
   github_ips = [
@@ -8,14 +8,43 @@ locals {
     "185.199.110.153",
     "185.199.111.153",
   ]
+
+  improvemx_records = [
+    {
+      value    = "mx1.improvmx.com"
+      priority = 10
+      type     = "MX"
+    },
+    {
+      value    = "mx2.improvmx.com"
+      priority = 20
+      type     = "MX"
+    },
+    {
+      value    = "v=spf1 include:spf.improvmx.com ~all"
+      priority = 0
+      type     = "TXT"
+    },
+  ]
 }
 
 // Create A records for the website hosted on GitHub.
 resource "cloudflare_record" "github" {
-  count  = length(local.github_ips)
-  name   = "@"
-  value  = element(local.github_ips, count.index)
-  ttl    = 300
-  type   = "A"
+  count   = length(local.github_ips)
+  name    = "@"
+  value   = element(local.github_ips, count.index)
+  ttl     = 300
+  type    = "A"
   zone_id = local.zone_id
+}
+
+// Create the email forwarding records.
+resource "cloudflare_record" "email_forwarding" {
+  count    = length(local.improvemx_records)
+  name     = "@"
+  value    = element(local.improvemx_records, count.index).value
+  ttl      = 3600
+  type     = element(local.improvemx_records, count.index).type
+  priority = element(local.improvemx_records, count.index).priority
+  zone_id  = local.zone_id
 }
--- binomi.al/provider.tf
+++ binomi.al/provider.tf
@@ -1,17 +1,9 @@
-variable "mailgun_api_key" {
-  type = string
-}
-
 variable "cloudflare_email" {
   type = string
 }
 
 variable "cloudflare_api_key" {
   type = string
-}
-
-provider "mailgun" {
-  api_key = var.mailgun_api_key
 }
 
 provider "cloudflare" {
