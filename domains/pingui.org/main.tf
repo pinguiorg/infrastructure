40f986643c8e975bfa4ad58f349d375bdbaee506
Lucas Servén Marín <lserven@gmail.com>
2021-02-26 15:09:04 +0100

    domains/pingui.org: verify domain for github
    Signed-off-by: Lucas Servén Marín <lserven@gmail.com>

--- domains/pingui.org/main.tf
+++ domains/pingui.org/main.tf
@@ -1,5 +1,7 @@
 locals {
-  domain = "pingui.org"
+  domain                     = "pingui.org"
+  github_verification        = "db3585dca6"
+  github_verification_domain = "_github-challenge-pinguiorg"
 }
 
 resource "cloudflare_zone" "pingui" {
@@ -16,4 +18,11 @@ resource "cloudflare_record" "delegate_aws" {
   name    = "aws"
   type    = "NS"
   value   = element(aws_route53_zone.pingui.name_servers, count.index)
+}
+
+resource "cloudflare_record" "github_verification" {
+  zone_id = cloudflare_zone.pingui.id
+  name    = local.github_verification_domain
+  type    = "TXT"
+  value   = local.github_verification
 }
