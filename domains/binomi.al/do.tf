e3ed8a519739afdcd514cb65a8b7d53ba486a457
Lucas Servén Marín <lserven@gmail.com>
2021-04-02 13:18:49 +0200

    domains/binomi-al: create bucket
    Signed-off-by: Lucas Servén Marín <lserven@gmail.com>

--- domains/binomi.al/do.tf
+++ domains/binomi.al/do.tf
@@ -1,3 +1,9 @@
 resource "digitalocean_project" "binomial" {
-  name        = "binomial"
+  name = "binomial"
+}
+
+resource "digitalocean_spaces_bucket" "food" {
+  name   = "food"
+  region = "fra1"
+  acl    = "private"
 }
