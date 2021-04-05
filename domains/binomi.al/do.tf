42a7e6741b77694909ecb745110f2d8fb183d335
Lucas Servén Marín <lserven@gmail.com>
2021-04-05 18:20:42 +0200

    domains/binomi.al: remove do
    Signed-off-by: Lucas Servén Marín <lserven@gmail.com>

--- domains/binomi.al/do.tf
+++ domains/binomi.al/do.tf
@@ -1,11 +1,4 @@
-resource "digitalocean_project" "binomial" {
-  name = "binomial"
-}
-
-resource "digitalocean_spaces_bucket" "food" {
-  name   = "food"
-  region = "fra1"
-  acl    = "private"
+provider "digitalocean" {
 }
 
 resource "scaleway_account_ssh_key" "squat" {
