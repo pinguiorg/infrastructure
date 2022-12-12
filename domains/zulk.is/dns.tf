5bd1bc2cc306ac4bcc74124374e61d6fab0f8c64
Lucas Servén Marín <lserven@gmail.com>
2022-12-12 19:29:09 +0100

    domains: zulk.is
    Signed-off-by: Lucas Servén Marín <lserven@gmail.com>

---
+++ domains/zulk.is/dns.tf
@@ -0,0 +1,53 @@
+locals {
+  domain = "zulk.is"
+
+  github_ips = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153",
+  ]
+
+  improvemx_records = [
+    {
+      value    = "mx1.improvmx.com"
+      priority = 10
+      type     = "MX"
+    },
+    {
+      value    = "mx2.improvmx.com"
+      priority = 20
+      type     = "MX"
+    },
+    {
+      value    = "v=spf1 include:spf.improvmx.com ~all"
+      priority = 0
+      type     = "TXT"
+    },
+  ]
+}
+
+resource "cloudflare_zone" "zulkis" {
+  zone = local.domain
+}
+
+// Create A records for the website hosted on GitHub.
+resource "cloudflare_record" "github" {
+  count   = length(local.github_ips)
+  name    = "@"
+  value   = element(local.github_ips, count.index)
+  ttl     = 300
+  type    = "A"
+  zone_id = cloudflare_zone.zulkis.id
+}
+
+// Create the email forwarding records.
+resource "cloudflare_record" "email_forwarding" {
+  count    = length(local.improvemx_records)
+  name     = "@"
+  value    = element(local.improvemx_records, count.index).value
+  ttl      = 3600
+  type     = element(local.improvemx_records, count.index).type
+  priority = element(local.improvemx_records, count.index).priority
+  zone_id  = cloudflare_zone.zulkis.id
+}
